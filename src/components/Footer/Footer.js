27c732dcf24b1974bc719aae79923b624cf8b718
Myron R Schippers Jr <dubtwee@gmail.com>
2021-03-17 18:36:59 -0500

    creating some global elements to support app layout

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="app-banner">
+      <p>&copy; Myron R Schippers Jr</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="app-banner">
+      <h1>Series Planner</h1>
+    </header>
+  );
+}
+
+export default Header;
