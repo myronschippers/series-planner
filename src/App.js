0976086d92def2ffe24f1808207843bec72bfe9f
Myron R Schippers Jr <dubtwee@gmail.com>
2021-03-19 00:04:20 -0500

    encapsulating all dialog behavior and layout inside of the AddPgLayout component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 // CUSTOM COMPONENTS
@@ -8,24 +7,10 @@ import PgGenerator from './components/PgGenerator/PgGenerator';
 import AddPgLayout from './components/AddPgLayout/AddPgLayout';
 
 // THEME CONFIGURATION
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-} from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { theme } from './theme/_root.config';
 
 function App() {
-  const isAddPageOpen = useSelector((store) => store.isAddPageOpen);
-  const dispatch = useDispatch();
-
-  function handleClose() {
-    dispatch({ type: 'CLOSE_ADD_PAGE' });
-  }
-
   return (
     <ThemeProvider theme={theme}>
       <div>
@@ -37,21 +22,7 @@ function App() {
 
         <Footer />
       </div>
-      <Dialog
-        fullWidth={true}
-        maxWidth="xs"
-        open={isAddPageOpen}
-        onClose={handleClose}
-        aria-labelledby="max-width-dialog-title"
-      >
-        <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-        </DialogActions>
-        <DialogTitle>Add New Page Layout</DialogTitle>
-        <DialogContent>
-          <AddPgLayout />
-        </DialogContent>
-      </Dialog>
+      <AddPgLayout />
     </ThemeProvider>
   );
 }
--- src/components/AddPgLayout/AddPgLayout.js
+++ src/components/AddPgLayout/AddPgLayout.js
@@ -1,17 +1,29 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-
-// CUSTOM COMPONENTS
-import Panel from '../Panel/Panel';
+import { useSelector, useDispatch } from 'react-redux';
 
 // MATERIAL-UI COMPONENTS
-import { TextField, Button } from '@material-ui/core';
+import {
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  IconButton,
+  Typography,
+} from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 
 function AddPgLayout() {
   const [newPage, setNewPage] = useState({
     title: '',
   });
   const dispatch = useDispatch();
+  const isAddPageOpen = useSelector((store) => store.isAddPageOpen);
+
+  function handleClose() {
+    dispatch({ type: 'CLOSE_ADD_PAGE' });
+  }
 
   function submitNewPageHandler(event) {
     event.preventDefault();
@@ -37,16 +49,40 @@ function AddPgLayout() {
   }
 
   return (
-    <form onSubmit={submitNewPageHandler}>
-      <TextField
-        label="Page Title"
-        required
-        value={newPage.title}
-        onChange={(event) => changeField('title', event)}
-      />
-
-      <Button type="submit">Add Page</Button>
-    </form>
+    <Dialog
+      fullWidth
+      maxWidth="xs"
+      open={isAddPageOpen}
+      onClose={handleClose}
+      aria-labelledby="max-width-dialog-title"
+    >
+      <DialogContent>
+        <form className="dialog-tight" onSubmit={submitNewPageHandler}>
+          <div className="dialog-tight__block dialog-tight__block--lean-right">
+            <IconButton onClick={handleClose} size="small">
+              <Close fontSize="small" />
+            </IconButton>
+          </div>
+          <div className="dialog-tight__block">
+            <Typography component="h2" variant="h6">
+              Add New Page Layout
+            </Typography>
+          </div>
+          <div className="dialog-tight__block">
+            <TextField
+              label="Page Title"
+              required
+              value={newPage.title}
+              fullWidth
+              onChange={(event) => changeField('title', event)}
+            />
+          </div>
+          <div className="dialog-tight__block dialog-tight__block--lean-right">
+            <Button type="submit">Add Page</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
 
