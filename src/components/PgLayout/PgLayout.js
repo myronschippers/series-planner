9ba65ad9289cd6748d992927b5ffea8b13903509
Myron R Schippers Jr <dubtwee@gmail.com>
2021-03-20 23:18:30 -0500

    adding animation for loading a new page layout

--- src/components/PgLayout/PgLayout.js
+++ src/components/PgLayout/PgLayout.js
@@ -1,8 +1,11 @@
+import { useSpring, animated } from 'react-spring';
+
 function PgLayout(props) {
   const { title } = props.pgInfo;
+  const animProps = useSpring({ opacity: 1, from: { opacity: 0 } });
 
   return (
-    <div className="pg-panel">
+    <div className="pg-panel" style={animProps}>
       <h2>{title}</h2>
     </div>
   );
